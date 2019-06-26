2a61e10c22ef783a32d936a30639b2c48b9fb39c
gihyeon <ken29511@gmail.com>
2019-06-26 20:18:27 +0900

    gihyeon 06/26 20:17
    ** 실수로 master로 작업
    category_discount.jsp
    : js부분 위로 이동
    goods_list.js
    : 카운트 부분 js 삭제

--- kokonutStationery/src/main/webapp/js/goods_list.js
+++ kokonutStationery/src/main/webapp/js/goods_list.js
@@ -1,11 +1,3 @@
 /*$('.category_name').click(function(){
 	alert($('.category_name').text());
 });*/
-
-//카운트다운 영역
-jQuery('#countDown').countdown({
-    date: '06/20/2019 23:59:59',
-    offset: 9,
-    day: 'Day',
-    days: 'Days'
-});
