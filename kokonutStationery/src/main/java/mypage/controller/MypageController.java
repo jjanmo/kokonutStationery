db64b605e065f4cbf64a2cf019e0f3b82782ed09
ssong <sing2song@naver.com>
2019-06-27 21:47:46 +0900

    mypageController 수정
    상품후기,문의,주문내역페이지와
    mypageController 연결

--- kokonutStationery/src/main/java/mypage/controller/MypageController.java
+++ kokonutStationery/src/main/java/mypage/controller/MypageController.java
@@ -1,21 +1,49 @@
 package mypage.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import user.dao.UserDAO;
+
 @Controller
 @RequestMapping("/mypage/*")
 public class MypageController {
-
+	
+	@Autowired	
+	private UserDAO userDAO;
+	
 	//주문내역/배송조회 페이지
 	@GetMapping("/mypage_orderlist.do")
 	public ModelAndView orderlist() {
+		
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("display", "/mypage/mypage_orderlist.jsp");
 		mav.setViewName("/main/nosIndex");
 		return mav;
 	}
-
+	
+	//상품후기
+	@GetMapping("/mypage_review.do")
+	public ModelAndView review() {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("display", "/mypage/mypage_review.jsp");
+		mav.setViewName("/main/nosIndex");
+		return mav;
+	}
+	
+	//상품문의
+	@GetMapping("/mypage_qna.do")
+	public ModelAndView qna() {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("display", "/mypage/mypage_qna.jsp");
+		mav.setViewName("/main/nosIndex");
+		return mav;
+	}
 }
