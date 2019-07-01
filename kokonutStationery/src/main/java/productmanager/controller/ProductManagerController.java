6c397356d77e0151b70d2d64c682b9fcb1c380fd
ssong <sing2song@naver.com>
2019-07-01 19:41:28 +0900

    productManagerController 수정
    detail,thumb 이미지 경로 수정

--- kokonutStationery/src/main/java/productmanager/controller/ProductManagerController.java
+++ kokonutStationery/src/main/java/productmanager/controller/ProductManagerController.java
@@ -50,8 +50,8 @@ public class ProductManagerController {
 		int seq = productManagerDAO.productCodeIncrease();		
 		System.out.println(seq);
 		//파일경로설정 - 옮겨받으시면 경로수정 꼭 해주세용
-		String thumbImgPath = "C:\\Users\\Jun\\git\\kokonutStationery\\kokonutStationery\\src\\main\\webapp\\image\\thumb"; 
-		String detailedImgPath = "C:\\Users\\Jun\\git\\kokonutStationery\\kokonutStationery\\src\\main\\webapp\\image\\detailed";
+		String thumbImgPath = "D:\\JavaWeb\\Git\\workspace\\kokonutStationery\\kokonutStationery\\src\\main\\webapp\\image\\thumb"; 
+		String detailedImgPath = "D:\\JavaWeb\\Git\\workspace\\kokonutStationery\\kokonutStationery\\src\\main\\webapp\\image\\detailed";
 		
 		//파일 이름 지정
 		String thumbImgName = seq+".jpg";
