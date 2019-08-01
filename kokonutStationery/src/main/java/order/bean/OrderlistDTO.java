907ccba943e1f1bd88a0b40d7fc00eaade0b5766
ssong <sing2song@naver.com>
2019-08-01 18:52:47 +0900

    crpayment수정, 주문리스트 충돌수정

--- kokonutStationery/src/main/java/order/bean/OrderlistDTO.java
+++ kokonutStationery/src/main/java/order/bean/OrderlistDTO.java
@@ -20,7 +20,7 @@ public class OrderlistDTO {
 	private int paymentType;			//결제방법 (1 : 신용카드 / 2 : 핸드폰)
 	private int deliveryFee; 
 	private int totalPayment;			//총결제금액 ( = 상품합계금액 + 배송비) 해당주문번호에 대한 총 결제금액
-	private int erPayment;				//취소/환불금액(cancel /refund) 둘다 있다면,합산금액으로
+	private int crPayment;				//취소/환불금액(cancel /refund) 둘다 있다면,합산금액으로
 	private int orderState;				//주문상태
 	private String erState;				//교환/환불상태
 	private String erReason;			//이유
--- kokonutStationery/src/main/java/order/dao/OrderlistDAO.java
+++ kokonutStationery/src/main/java/order/dao/OrderlistDAO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package order.dao;
 
 import java.util.List;
@@ -25,28 +24,4 @@ public interface OrderlistDAO {
 			String productCodeStr, String optionContentStr,	String changeRefundQtyStr);
 
 }
-=======
-package order.dao;
 
-import java.util.List;
-import java.util.Map;
-
-import order.bean.OrderlistDTO;
-
-public interface OrderlistDAO {
-
-	public List<OrderlistDTO> mypage_orderlist(Map<String, String> map);
-
-	public int getTotalAS(Map<String, String> map);
-
-	public void orderCancel(Map<String,String> map);
-
-	public void orderChange(String orderCode);
-
-	public void orderRefund(String orderCode);
-
-	public void orderReceipt(String orderCode);
-
-
-}
->>>>>>> branch 'master' of https://github.com/jjanmo/kokonutStationery.git
